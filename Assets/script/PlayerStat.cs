93abeb7679c649227df08c766dc242265997d84f
Shashinda Adithya <142221873+Shashinda2001@users.noreply.github.com>
2026-02-14 11:52:57 +0530

    implement PlayerStat script with coin collection and traffic data logging

---
+++ Assets/script/PlayerStat.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class PlayerStat : MonoBehaviour
+{
+    [Header("Player Statistics")]
+    public int coinCount = 0; // This stores your total coins
+
+   // [Header("Player Time")]
+    public string savedTime;
+
+    [Header("Traffic Data")]
+    public float speedAtTrigger;
+
+
+    // Other scripts will call this function
+    public void AddCoin(int amount)
+    {
+        coinCount += amount;
+        Debug.Log("Coins Collected: " + coinCount);
+    }
+
+    //public void UpdateSavedTime(string newTime)
+    //{
+    //    savedTime = newTime;
+    //    Debug.Log("Time saved in PlayerStat: " + savedTime);
+    //}
+
+   
+    public void SaveTrafficData(string time, float speed)
+    {
+        savedTime = time;
+        speedAtTrigger = speed;
+        Debug.Log("Traffic Check! Time: " + time + " | Speed: " + speed.ToString("F1") + " KMH");
+    }
+}
+
+
+
